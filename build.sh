d291fa67157dda1040ed0ee850d2707b3e31111f
benbenhuo <157685179+benbenhuo@users.noreply.github.com>
2024-06-24 13:45:19 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 sudo apt install -y gcc make gettext bison flex bc zlib1g-dev libncurses5-dev lzma libssl-dev unzip
 wget https://raw.githubusercontent.com/benbenhuo/debugger-action/master/hi3798mv100_defconfig
-wget https://codeload.github.com/tegzwn/HiSTBLinuxV100R005C00SPC050/zip/refs/heads/master.zip
+wget https://codeload.github.com/benbenhuo/HiSTBLinuxV100R005C00SPC050/zip/refs/heads/master
 wget https://raw.githubusercontent.com/benbenhuo/debugger-action/master/cfg.mak
-unzip master.zip
-cp hi3798mv100_defconfig HiSTBLinuxV100R005C00SPC050/source/kernel/linux-4.4.y/arch/arm/configs/hi3798mv100_defconfig
-cp cfg.mak HiSTBLinuxV100R005C00SPC050/cfg.mak
-cd HiSTBLinuxV100R005C00SPC050
+unzip master
+cp hi3798mv100_defconfig HiSTBLinuxV100R005C00SPC050-master/source/kernel/linux-4.4.y/arch/arm/configs/hi3798mv100_defconfig
+cp cfg.mak HiSTBLinuxV100R005C00SPC050-master/cfg.mak
+cd HiSTBLinuxV100R005C00SPC050-master
 source ./env.sh
-make build -j$(nproc)
+cd source/kernel/linux-4.4.y
+make ARCH=arm hi3798mv100_defconfig
+make O=out ARCH=arm modules -j$(nproc)
